eb36fa6993739228753b488485e75a58e0c11808
TheLordBlarg <jeremyrath@ymail.com>
2017-05-31 00:17:18 -0500

    Add files via upload

---
+++ 2013-2014.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import database_functions_helper as dfh
+import database_functions_user as dfu
+import unidecode
+import lxml.html
+import requests
+import numpy
+import time
+import os
+
+
+
+#"""
+databases = ["sofifa_database.txt"]; i = 1
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+while i > 0:
+    try:
+        dfu.results_processor("match_list_eng-premier-league_2013-2014.txt",
+                              "sofifa_database_14.txt",
+                              average = 'position',
+                              status = 10,
+                              ignoreplayers = 7,
+                              keystats = True,
+                              databases = databases)
+    except requests.exceptions.ConnectionError:
+        print "Error!"
+        time.sleep(10)
+#"""
